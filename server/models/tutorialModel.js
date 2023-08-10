45b9e4d6f494fe70a600b32183212254e4763e53
MarkusNodeJs <55816864+MarkusTech@users.noreply.github.com>
2023-08-10 14:22:52 +0800

    Update tutorialModel.js

--- server/models/tutorialModel.js
+++ server/models/tutorialModel.js
@@ -1,5 +1,38 @@
 import mongoose from "mongoose";
 
-const tutorialSchema = new mongoose.Schema({}, { timestamps: true });
+const tutorialSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    slug: {
+      required: true,
+      type: String,
+      unique: true,
+      index: true,
+    },
+    tutorialCategory: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "TutorialCategory",
+      required: true,
+    },
+    topicName: {
+      required: true,
+      unique: true,
+      type: String,
+    },
+    content: {
+      required: true,
+      type: String,
+    },
+    keywords: {
+      type: [],
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
 
 export default mongoose.Model("Tutorial", tutorialSchema);

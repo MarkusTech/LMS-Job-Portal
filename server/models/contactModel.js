0627e3347df3fc1b0425a0e88a6d58935daaa844
MarkusNodeJs <55816864+MarkusTech@users.noreply.github.com>
2023-08-11 14:27:07 +0800

    contact model

---
+++ server/models/contactModel.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+// Declare the Schema of the Mongo model
+const contactSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    mobile: {
+      type: String,
+      required: true,
+    },
+    subject: {
+      type: String,
+      required: true,
+    },
+    profession: {
+      ype: String,
+      required: true,
+    },
+    comment: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+//Export the model
+export default mongoose.model("Contact", contactSchema);

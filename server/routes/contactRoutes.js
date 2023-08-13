4e65f3d909f5ded3217a737d25975600f11ff206
Wenn Mark Recopelacion <129263583+MarkusNodeJs@users.noreply.github.com>
2023-08-13 20:05:29 +0800

    Update contactRoutes.js

--- server/routes/contactRoutes.js
+++ server/routes/contactRoutes.js
@@ -8,3 +8,11 @@ import {
   deleteContact,
   updateRContactStatus,
 } from "../controllers/contactController.js";
+
+router.post("/", createContact);
+router.get("/", getAllContact);
+router.get("/:id", getAContact);
+router.delete("/:id", deleteContact);
+router.put("/:id", updateRContactStatus);
+
+export default router;

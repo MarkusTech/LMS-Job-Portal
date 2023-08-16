9e7eeea22af0f5dacb2362b5307586b3677a0e43
Wenn Mark Recopelacion <55816864+MarkusTech@users.noreply.github.com>
2023-08-16 14:10:06 +0800

    Update contactRoutes.js

--- server/routes/contactRoutes.js
+++ server/routes/contactRoutes.js
@@ -10,10 +10,17 @@ import {
   updateRContactStatus,
 } from "../controllers/contactController.js";
 
+//** POST METHOD */
 router.post("/", authMiddleware, createContact);
+
+//** GET METHOD */
 router.get("/", authMiddleware, getAllContact);
 router.get("/:id", authMiddleware, restrictTo("admin"), getAContact);
+
+//** DELETE METHOD */
 router.delete("/:id", authMiddleware, restrictTo("admin"), deleteContact);
+
+//** PUT METHOD */
 router.put("/:id", authMiddleware, restrictTo("admin"), updateRContactStatus);
 
 export default router;

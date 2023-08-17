85678f05cee35a2751c75452e0d928788f2c7aa6
Wenn Mark Recopelacion <55816864+MarkusTech@users.noreply.github.com>
2023-08-17 09:18:53 +0800

    Update docCatRoutes.js

--- server/routes/docCatRoutes.js
+++ server/routes/docCatRoutes.js
@@ -9,10 +9,17 @@ import {
   updateDocCategory,
 } from "../controllers/docCatController.js";
 
+//** POST METHOD */
 router.post("/", authMiddleware, restrictTo("admin"), postDocCategory);
+
+//** GET METHOD */
 router.get("/:slug", authMiddleware, restrictTo("admin"), getADocCategory);
 router.get("/", getAllDocCatCategory);
+
+//** DELETE METHOD */
 router.delete("/:id", authMiddleware, restrictTo("admin"), deleteDocCategory);
+
+//** PUT METHOD */
 router.put("/:id", authMiddleware, restrictTo("admin"), updateDocCategory);
 
 export default router;

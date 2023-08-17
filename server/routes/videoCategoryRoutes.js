2ae3cfe439bb235486e81c254ce0bdd3f6163037
Wenn Mark Recopelacion <55816864+MarkusTech@users.noreply.github.com>
2023-08-17 09:16:42 +0800

    Update videoCategoryRoutes.js

--- server/routes/videoCategoryRoutes.js
+++ server/routes/videoCategoryRoutes.js
@@ -9,11 +9,19 @@ import {
   updateVideoCategory,
 } from "../controllers/videoCategoryController.js";
 
+//** GET METHOD */
 router.get("/all", authMiddleware, restrictTo("admin"), getAllVideoCategory);
 
+//** POST METHOD */
 router.post("/", authMiddleware, restrictTo("admin"), postVideoCategory);
+
+//** GET METHOD */
 router.get("/:slug", authMiddleware, restrictTo("admin"), getVideoCategory);
+
+//** DELETE METHOD */
 router.delete("/:id", authMiddleware, restrictTo("admin"), deleteVideoCategory);
+
+//** PUT METHOD */
 router.put("/:id", authMiddleware, restrictTo("admin"), updateVideoCategory);
 
 export default router;

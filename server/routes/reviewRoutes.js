f984185058cd6140cf89ec790a4f3fbbd5cc0bac
Wenn Mark Recopelacion <55816864+MarkusTech@users.noreply.github.com>
2023-08-17 09:18:07 +0800

    Update reviewRoutes.js

--- server/routes/reviewRoutes.js
+++ server/routes/reviewRoutes.js
@@ -9,10 +9,17 @@ import {
 } from "../controllers/reviewController.js";
 import { authMiddleware, restrictTo } from "../middlewares/authMiddleware.js";
 
+//** POST METHOD */
 router.post("/", authMiddleware, createReview);
+
+//** GET METHOD */
 router.get("/", authMiddleware, getAllReviews);
 router.get("/:id", authMiddleware, restrictTo("admin"), getAReview);
+
+//** DELETE METHOD */
 router.delete("/:id", authMiddleware, restrictTo("admin"), deleteReview);
+
+//** PUT METHOD */
 router.put("/:id", authMiddleware, restrictTo("admin"), updateReviewStatus);
 
 export default router;

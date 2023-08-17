d689c799984391645c16541a49698ca50f0c8a23
Wenn Mark Recopelacion <55816864+MarkusTech@users.noreply.github.com>
2023-08-17 09:15:41 +0800

    Update videoRoutes.js

--- server/routes/videoRoutes.js
+++ server/routes/videoRoutes.js
@@ -9,7 +9,7 @@ import {
   updateVideo,
 } from "../controllers/videoController.js";
 
-//** POST METHOD */
+//** POST METHOD  WITH ADMIN ROLE */
 router.post("/", authMiddleware, restrictTo("admin"), videoController);
 
 //** GET METHOD */
@@ -19,7 +19,7 @@ router.get("/", getAllVideo);
 //** DELETE METHOD */
 router.delete("/:id", deleteVideo);
 
-//** PUT METHOD */
+//** PUT METHOD WITH ADMIN ROLE */
 router.put("/:id", authMiddleware, restrictTo("admin"), updateVideo);
 
 export default router;

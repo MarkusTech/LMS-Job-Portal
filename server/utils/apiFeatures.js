01f49c6a8fbdbc3be55f7244716690d8e5f4e1aa
Wenn Mark Recopelacion <55816864+MarkusTech@users.noreply.github.com>
2023-08-18 09:59:46 +0800

    Update apiFeatures.js

--- server/utils/apiFeatures.js
+++ server/utils/apiFeatures.js
@@ -13,4 +13,31 @@ class APIFeatures {
     this.query = this.query.find(JSON.parse(queryStr));
     return this;
   }
+  sort() {
+    if (this.queryString.sort) {
+      const sortBy = this.query.sort.split(",").join(" ");
+      this.query = this.query.sort(sortBy);
+    } else {
+      this.query = this.query.sort("-createdAt");
+    }
+    return this;
+  }
+  limitFields() {
+    if (this.queryString.fields) {
+      const fields = this.query.fields.split(",").join(" ");
+      this.query = this.query.select(fields);
+    } else {
+      this.query = this.query.select("-__v");
+    }
+    return this;
+  }
+  paginate() {
+    const page = this.queryString.page * 1 || 1;
+    const limit = this.queryString.limit * 1 || 10;
+    const skip = (page - 1) * limit;
+    this.query = this.query.skip(skip).limit(limit);
+    return this;
+  }
 }
+
+export default APIFeatures;

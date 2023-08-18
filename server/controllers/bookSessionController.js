554ffd3f2d4fa89779522bd84d205d90888175fb
Wenn Mark Recopelacion <55816864+MarkusTech@users.noreply.github.com>
2023-08-18 10:04:09 +0800

    Update bookSessionController.js

--- server/controllers/bookSessionController.js
+++ server/controllers/bookSessionController.js
@@ -1,1 +1,16 @@
 import sesssionModel from "../models/sesssionModel";
+import { createOne, updateOne, deleteOne, getOne, getAll } from "./customController.js"
+
+const postSession = createOne(BookSession);
+const updateSession = updateOne(BookSession);
+const deleteSession = deleteOne(BookSession);
+const getASession = getOne(BookSession);
+const getAllSession = getAll(BookSession);
+
+export {
+  postSession,
+  updateSession,
+  deleteSession,
+  getASession,
+  getAllSession,
+};
